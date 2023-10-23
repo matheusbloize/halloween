5ceeea97b667f8ed2424c3cc3436fe2790303dd2
Matheus Bloize <mbloizepd@gmail.com>
2023-10-23 12:19:17 -0300

    refactor: tic tac toe logic refactored

--- src/components/TicTacToe.tsx
+++ src/components/TicTacToe.tsx
@@ -1,6 +1,10 @@
 "use client";
 
-import { useState } from "react";
+import Image from "next/image";
+import { useRef, useState } from "react";
+
+import backgroundImage from "@/assets/images/tictactoe/background.webp";
+import witchImage from "@/assets/images/tictactoe/witch.webp";
 
 interface Player {
   name: string;
@@ -8,258 +12,182 @@ interface Player {
 }
 
 const player1: Player = {
-  name: "player1",
-  symbol: "X",
+  name: "bat",
+  symbol: "static/images/tictactoe/bat.webp",
 };
 
 const player2: Player = {
-  name: "player2",
-  symbol: "O",
+  name: "jack",
+  symbol: "static/images/tictactoe/jack.webp",
 };
 
+type Win = [number, number, number];
+
+const win1: Win = [0, 1, 2];
+const win2: Win = [0, 3, 6];
+const win3: Win = [0, 4, 8];
+const win4: Win = [1, 4, 7];
+const win5: Win = [2, 4, 6];
+const win6: Win = [2, 5, 8];
+const win7: Win = [3, 4, 5];
+const win8: Win = [6, 7, 8];
+const winPossibilities = [win1, win2, win3, win4, win5, win6, win7, win8];
+
 const TicTacToe = () => {
+  const witchImageRef = useRef<HTMLImageElement | null>(null);
+  const witchAudioRef = useRef<HTMLAudioElement | null>(null);
   const [actualTurn, setActualTurn] = useState<Player>(player1);
   const [winner, setWinner] = useState("");
 
   const resetGame = () => {
     const section = document.getElementById("section")!;
     for (let i = 0; i < section.children.length; i++) {
-      (section.children[i].children[0] as HTMLSpanElement).innerText = "";
+      const img = section.children[i].children[0] as HTMLImageElement;
+      img.alt = "background";
+      img.srcset = "";
+      img.style.opacity = "0";
       (section.children[i] as HTMLDivElement).setAttribute(
         "selected-area",
         "false",
       );
-      (section.children[i] as HTMLDivElement).style.cursor = "pointer";
-      (section.children[i] as HTMLDivElement).style.pointerEvents = "auto";
+      img.style.cursor = "pointer";
     }
     setWinner("");
   };
 
   const checkWin = (currentTarget: HTMLElement) => {
-    for (let i = 0; i < currentTarget.children.length; i++) {
-      if (
-        ((currentTarget.children[0].children[0] as HTMLSpanElement)
-          .innerText === "X" &&
-          (currentTarget.children[1].children[0] as HTMLSpanElement)
-            .innerText === "X" &&
-          (currentTarget.children[2].children[0] as HTMLSpanElement)
-            .innerText === "X") ||
-        ((currentTarget.children[0].children[0] as HTMLSpanElement)
-          .innerText === "X" &&
-          (currentTarget.children[3].children[0] as HTMLSpanElement)
-            .innerText === "X" &&
-          (currentTarget.children[6].children[0] as HTMLSpanElement)
-            .innerText === "X") ||
-        ((currentTarget.children[0].children[0] as HTMLSpanElement)
-          .innerText === "X" &&
-          (currentTarget.children[4].children[0] as HTMLSpanElement)
-            .innerText === "X" &&
-          (currentTarget.children[8].children[0] as HTMLSpanElement)
-            .innerText === "X") ||
-        ((currentTarget.children[1].children[0] as HTMLSpanElement)
-          .innerText === "X" &&
-          (currentTarget.children[4].children[0] as HTMLSpanElement)
-            .innerText === "X" &&
-          (currentTarget.children[7].children[0] as HTMLSpanElement)
-            .innerText === "X") ||
-        ((currentTarget.children[2].children[0] as HTMLSpanElement)
-          .innerText === "X" &&
-          (currentTarget.children[4].children[0] as HTMLSpanElement)
-            .innerText === "X" &&
-          (currentTarget.children[6].children[0] as HTMLSpanElement)
-            .innerText === "X") ||
-        ((currentTarget.children[2].children[0] as HTMLSpanElement)
-          .innerText === "X" &&
-          (currentTarget.children[5].children[0] as HTMLSpanElement)
-            .innerText === "X" &&
-          (currentTarget.children[8].children[0] as HTMLSpanElement)
-            .innerText === "X") ||
-        ((currentTarget.children[3].children[0] as HTMLSpanElement)
-          .innerText === "X" &&
-          (currentTarget.children[4].children[0] as HTMLSpanElement)
-            .innerText === "X" &&
-          (currentTarget.children[5].children[0] as HTMLSpanElement)
-            .innerText === "X") ||
-        ((currentTarget.children[6].children[0] as HTMLSpanElement)
-          .innerText === "X" &&
-          (currentTarget.children[7].children[0] as HTMLSpanElement)
-            .innerText === "X" &&
-          (currentTarget.children[8].children[0] as HTMLSpanElement)
-            .innerText === "X")
-      ) {
-        setWinner(player1.name);
-        setActualTurn(player1);
-        (currentTarget.children[i] as HTMLDivElement).style.pointerEvents =
-          "none";
-      } else if (
-        ((currentTarget.children[0].children[0] as HTMLSpanElement)
-          .innerText === "O" &&
-          (currentTarget.children[1].children[0] as HTMLSpanElement)
-            .innerText === "O" &&
-          (currentTarget.children[2].children[0] as HTMLSpanElement)
-            .innerText === "O") ||
-        ((currentTarget.children[0].children[0] as HTMLSpanElement)
-          .innerText === "O" &&
-          (currentTarget.children[3].children[0] as HTMLSpanElement)
-            .innerText === "O" &&
-          (currentTarget.children[6].children[0] as HTMLSpanElement)
-            .innerText === "O") ||
-        ((currentTarget.children[0].children[0] as HTMLSpanElement)
-          .innerText === "O" &&
-          (currentTarget.children[4].children[0] as HTMLSpanElement)
-            .innerText === "O" &&
-          (currentTarget.children[8].children[0] as HTMLSpanElement)
-            .innerText === "O") ||
-        ((currentTarget.children[1].children[0] as HTMLSpanElement)
-          .innerText === "O" &&
-          (currentTarget.children[4].children[0] as HTMLSpanElement)
-            .innerText === "O" &&
-          (currentTarget.children[7].children[0] as HTMLSpanElement)
-            .innerText === "O") ||
-        ((currentTarget.children[2].children[0] as HTMLSpanElement)
-          .innerText === "O" &&
-          (currentTarget.children[4].children[0] as HTMLSpanElement)
-            .innerText === "O" &&
-          (currentTarget.children[6].children[0] as HTMLSpanElement)
-            .innerText === "O") ||
-        ((currentTarget.children[2].children[0] as HTMLSpanElement)
-          .innerText === "O" &&
-          (currentTarget.children[5].children[0] as HTMLSpanElement)
-            .innerText === "O" &&
-          (currentTarget.children[8].children[0] as HTMLSpanElement)
-            .innerText === "O") ||
-        ((currentTarget.children[3].children[0] as HTMLSpanElement)
-          .innerText === "O" &&
-          (currentTarget.children[4].children[0] as HTMLSpanElement)
-            .innerText === "O" &&
-          (currentTarget.children[5].children[0] as HTMLSpanElement)
-            .innerText === "O") ||
-        ((currentTarget.children[6].children[0] as HTMLSpanElement)
-          .innerText === "O" &&
-          (currentTarget.children[7].children[0] as HTMLSpanElement)
-            .innerText === "O" &&
-          (currentTarget.children[8].children[0] as HTMLSpanElement)
-            .innerText === "O")
-      ) {
-        setWinner(player2.name);
-        setActualTurn(player1);
-        (currentTarget.children[i] as HTMLDivElement).style.pointerEvents =
-          "none";
-      } else if (
-        (currentTarget.children[0].children[0] as HTMLSpanElement).innerText !==
-          "" &&
-        (currentTarget.children[1].children[0] as HTMLSpanElement).innerText !==
-          "" &&
-        (currentTarget.children[2].children[0] as HTMLSpanElement).innerText !==
-          "" &&
-        (currentTarget.children[3].children[0] as HTMLSpanElement).innerText !==
-          "" &&
-        (currentTarget.children[4].children[0] as HTMLSpanElement).innerText !==
-          "" &&
-        (currentTarget.children[5].children[0] as HTMLSpanElement).innerText !==
-          "" &&
-        (currentTarget.children[6].children[0] as HTMLSpanElement).innerText !==
-          "" &&
-        (currentTarget.children[7].children[0] as HTMLSpanElement).innerText !==
-          "" &&
-        (currentTarget.children[8].children[0] as HTMLSpanElement).innerText !==
-          ""
-      ) {
-        setWinner("tie");
+    for (let i = 0; i < 9; i++) {
+      if (winPossibilities[i]) {
+        if (
+          (
+            currentTarget.children[winPossibilities[i][0]]
+              .children[0] as HTMLImageElement
+          ).alt === "bat" &&
+          (
+            currentTarget.children[winPossibilities[i][1]]
+              .children[0] as HTMLImageElement
+          ).alt === "bat" &&
+          (
+            currentTarget.children[winPossibilities[i][2]]
+              .children[0] as HTMLImageElement
+          ).alt === "bat"
+        ) {
+          setWinner(player1.name);
+          setActualTurn(player1);
+          return;
+        }
+        if (
+          (
+            currentTarget.children[winPossibilities[i][0]]
+              .children[0] as HTMLImageElement
+          ).alt === "jack" &&
+          (
+            currentTarget.children[winPossibilities[i][1]]
+              .children[0] as HTMLImageElement
+          ).alt === "jack" &&
+          (
+            currentTarget.children[winPossibilities[i][2]]
+              .children[0] as HTMLImageElement
+          ).alt === "jack"
+        ) {
+          setWinner(player2.name);
+          setActualTurn(player2);
+          return;
+        }
+        if (
+          (currentTarget.children[0].children[0] as HTMLImageElement).alt !==
+            "background" &&
+          (currentTarget.children[1].children[0] as HTMLImageElement).alt !==
+            "background" &&
+          (currentTarget.children[2].children[0] as HTMLImageElement).alt !==
+            "background" &&
+          (currentTarget.children[3].children[0] as HTMLImageElement).alt !==
+            "background" &&
+          (currentTarget.children[4].children[0] as HTMLImageElement).alt !==
+            "background" &&
+          (currentTarget.children[5].children[0] as HTMLImageElement).alt !==
+            "background" &&
+          (currentTarget.children[6].children[0] as HTMLImageElement).alt !==
+            "background" &&
+          (currentTarget.children[7].children[0] as HTMLImageElement).alt !==
+            "background" &&
+          (currentTarget.children[8].children[0] as HTMLImageElement).alt !==
+            "background"
+        ) {
+          setWinner("tie");
+        }
       }
     }
   };
 
+  const showWitch = () => {
+    if (witchImageRef.current && witchAudioRef.current) {
+      witchImageRef.current.style.display = "block";
+      witchImageRef.current.style.animation = "witch 1s ease";
+      const audio = document.querySelector("#witch-audio") as HTMLAudioElement;
+      audio.play();
+    }
+  };
+
+  const hideWitch = () => {
+    if (witchImageRef.current && witchAudioRef.current) {
+      witchImageRef.current.style.display = "none";
+      witchImageRef.current.style.animation = "";
+    }
+  };
+
   const handlePlay = (e: React.MouseEvent<HTMLDivElement>) => {
-    if ((e.target as HTMLElement).tagName === "DIV") {
-      if ((e.target as HTMLElement).getAttribute("selected-area") === "false") {
-        const span = (e.target as HTMLElement).children[0] as HTMLSpanElement;
-        span.innerText = actualTurn.symbol;
-        (e.target as HTMLElement).setAttribute("selected-area", "true");
-        (e.target as HTMLElement).style.cursor = "not-allowed";
+    if ((e.target as HTMLElement).tagName === "IMG") {
+      if (
+        (e.target as HTMLElement).parentElement!.getAttribute(
+          "selected-area",
+        ) === "false"
+      ) {
+        const img = e.target as HTMLElement as HTMLImageElement;
+        img.srcset = `/${actualTurn.symbol}`;
+        img.style.opacity = "1";
+        img.style.cursor = "not-allowed";
+        actualTurn.name === "bat" ? (img.alt = "bat") : (img.alt = "jack");
+        (e.target as HTMLElement).parentElement!.setAttribute(
+          "selected-area",
+          "true",
+        );
         checkWin(e.currentTarget);
-        actualTurn.name === "player1"
+        actualTurn.name === "bat"
           ? setActualTurn(player2)
           : setActualTurn(player1);
       } else {
-        alert("boo");
+        showWitch();
       }
     } else {
-      alert("boo");
+      showWitch();
     }
   };
 
   return (
     <article className="max-w-[75%] mx-auto flex flex-col items-center gap-[2em]">
-      <h1 className="text-center text-[2em]">TicTacToe</h1>
+      <h1 className="text-center text-[2em]">Tic Tac Toe</h1>
       <section
         id="section"
         onClick={handlePlay}
-        className="grid grid-cols-3 gap-0 w-fit min-w-[15em] mb-[4em]"
+        className="grid grid-cols-3 gap-0 w-fit min-w-[15em]"
       >
-        <div
-          selected-area="false"
-          area-number="1"
-          className="bg-[#FF6400] cursor-pointer transition-all hover:bg-[#ff660080] w-[5em] h-[5em] border-solid border-[#010101] border-[.2em] border-l-0 border-t-0 flex items-center justify-center"
-        >
-          <span className="text-[#010101] text-[3em]"></span>
-        </div>
-        <div
-          selected-area="false"
-          area-number="2"
-          className="bg-[#FF6400] cursor-pointer transition-all hover:bg-[#ff660080] w-[5em] h-[5em] border-solid border-[#010101] border-[.2em] border-t-0 border-r-0 border-l-0 flex items-center justify-center"
-        >
-          <span className="text-[#010101] text-[3em]"></span>
-        </div>
-        <div
-          selected-area="false"
-          area-number="3"
-          className="bg-[#FF6400] cursor-pointer transition-all hover:bg-[#ff660080] w-[5em] h-[5em] border-solid border-[#010101] border-[.2em] border-t-0 border-r-0 flex items-center justify-center"
-        >
-          <span className="text-[#010101] text-[3em]"></span>
-        </div>
-        <div
-          selected-area="false"
-          area-number="4"
-          className="bg-[#FF6400] cursor-pointer transition-all hover:bg-[#ff660080] w-[5em] h-[5em] border-solid border-[#010101] border-[.2em] border-l-0 border-t-0 border-b-0 flex items-center justify-center"
-        >
-          <span className="text-[#010101] text-[3em]"></span>
-        </div>
-        <div
-          selected-area="false"
-          area-number="5"
-          className="bg-[#FF6400] cursor-pointer transition-all hover:bg-[#ff660080] w-[5em] h-[5em] flex items-center justify-center"
-        >
-          <span className="text-[#010101] text-[3em]"></span>
-        </div>
-        <div
-          selected-area="false"
-          area-number="6"
-          className="bg-[#FF6400] cursor-pointer transition-all hover:bg-[#ff660080] w-[5em] h-[5em] border-solid border-[#010101] border-[.2em] border-t-0 border-r-0 border-b-0 flex items-center justify-center"
-        >
-          <span className="text-[#010101] text-[3em]"></span>
-        </div>
-        <div
-          selected-area="false"
-          area-number="7"
-          className="bg-[#FF6400] cursor-pointer transition-all hover:bg-[#ff660080] w-[5em] h-[5em] border-solid border-[#010101] border-[.2em] border-l-0 border-b-0 flex items-center justify-center"
-        >
-          <span className="text-[#010101] text-[3em]"></span>
-        </div>
-        <div
-          selected-area="false"
-          area-number="8"
-          className="bg-[#FF6400] cursor-pointer transition-all hover:bg-[#ff660080] w-[5em] h-[5em] border-solid border-[#010101] border-[.2em] border-l-0 border-b-0 border-r-0 flex items-center justify-center"
-        >
-          <span className="text-[#010101] text-[3em]"></span>
-        </div>
-        <div
-          selected-area="false"
-          area-number="9"
-          className="bg-[#FF6400] cursor-pointer transition-all hover:bg-[#ff660080] w-[5em] h-[5em] border-solid border-[#010101] border-[.2em] border-r-0 border-b-0 flex items-center justify-center"
-        >
-          <span className="text-[#010101] text-[3em]"></span>
-        </div>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((_, index) => (
+          <div
+            key={index + 1}
+            selected-area="false"
+            area-number={index + 1}
+            className={`${
+              winner !== "" ? "pointer-events-none" : "pointer-events-auto"
+            } bg-[#FF6400] hover:bg-[#ff660080] transition-all w-[5em] h-[5em] border-solid border-[#010101] border-[.2em] border-l-0 border-t-0 flex items-center justify-center z-10`}
+          >
+            <Image
+              className="opacity-0 transition-all cursor-pointer hover:bg-[#ff660080] h-[-webkit-fill-available] w-[-webkit-fill-available]"
+              src={backgroundImage}
+              alt="background"
+            />
+          </div>
+        ))}
       </section>
       <div
         className={`${
@@ -269,17 +197,29 @@ const TicTacToe = () => {
         {winner === "tie" && <span>Tie game!</span>}
         {winner !== "" && winner !== "tie" && (
           <span>
-            {" "}
-            Congrats! <br /> the Winner is: {winner}
+            Congrats! <br /> the Winner is:{" "}
+            <span className="text-[#ffffff]">{winner}</span>
           </span>
         )}
         <button
-          className="transition-all hover:text-[#ff660080]"
+          className="transition-all hover:text-[#ff660080] z-10"
           onClick={resetGame}
         >
           Play again!
         </button>
       </div>
+      <Image
+        ref={witchImageRef}
+        onAnimationEnd={hideWitch}
+        className="z-0 invert rotate-[-7deg] absolute bottom-0 left-0 w-[35vw] hidden"
+        src={witchImage}
+        alt="witch"
+      />
+      <audio
+        id="witch-audio"
+        ref={witchAudioRef}
+        src="../static/audio/tictactoe/witch.MP3"
+      ></audio>
     </article>
   );
 };
